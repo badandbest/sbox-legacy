dc9899c75ae0727e56619b5559ceea2bd59907f2
badandbest <91832803+badandbest@users.noreply.github.com>
2025-05-25 22:42:14 +0200

    Entity lifecycle

--- Code/Entities/Base/Entity.cs
+++ Code/Entities/Base/Entity.cs
@@ -28,7 +28,49 @@ public partial class Entity : IEntity
 		GameObject = new GameObject( true, GetType().Name );
 		GameObject.Flags |= GameObjectFlags.NotSaved;
 
+		var wrapper = GameObject.AddComponent<EntityWrapper>();
+		wrapper.Entity = this;
+
 		All.Add( this );
+	}
+
+	public bool IsValid => GameObject.IsValid();
+
+	/// <summary>
+	/// Called when the entity is spawned in. It should have all properties set by this point.
+	/// This is the place to set up your entity.
+	/// </summary>
+	public virtual void Spawn() { }
+
+	/// <summary>
+	/// Called when simulating as part of a player's tick. Like if it's a pawn.
+	/// </summary>
+	public virtual void Simulate( IClient cl ) { }
+
+	/// <summary>
+	/// Called each frame clientside only on Pawn (and anything the pawn decides to call it on).
+	/// </summary>
+	public virtual void FrameSimulate( IClient cl ) { }
+
+	/// <summary>
+	/// Pawns get a chance to mess with the input. This is called on the client.
+	/// </summary>
+	public virtual void BuildInput() { }
+
+	/// <summary>
+	/// Called when the entity was destroyed. This is not the same as the class destructor.
+	/// </summary>
+	protected virtual void OnDestroy() { }
+
+	/// <summary>
+	/// Delete this entity. You shouldn't access it anymore.
+	/// </summary>
+	public void Delete()
+	{
+		OnDestroy();
+
+		All.Remove( this );
+		GameObject.Destroy();
 	}
 
 	#region Unimplemented
