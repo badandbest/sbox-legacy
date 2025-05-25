694b4f84649c53cfd1da8aef3a9bb573240ffe24
badandbest <91832803+badandbest@users.noreply.github.com>
2025-05-26 00:15:25 +0200

    Entity rendering options (unimplemented)

---
+++ Code/Entities/Base/Entity.Rendering.cs
@@ -0,0 +1,34 @@
+﻿namespace Legacy;
+
+public partial class Entity
+{
+	/// <summary>
+	/// Turning this off will completely prevent the entity from drawing
+	/// </summary>
+	[Category( "Rendering" )]
+	public bool EnableDrawing { get; set; }
+
+	/// <summary>
+	/// Don't cast no shadow
+	/// </summary>
+	[Category( "Rendering" )]
+	public bool EnableShadowCasting { get; set; }
+
+	/// <summary>
+	/// Render Shadows when hidden due to being in first person
+	/// </summary>
+	[Category( "Rendering" )]
+	public bool EnableShadowInFirstPerson { get; set; }
+
+	/// <summary>
+	/// Hide this model when in first person, or our parent is in first person
+	/// </summary>
+	[Category( "Rendering" )]
+	public bool EnableHideInFirstPerson { get; set; }
+
+	/// <summary>
+	/// Enable Viewmodel Rendering
+	/// </summary>
+	[Category( "Rendering" )]
+	public bool EnableViewmodelRendering { get; set; }
+}
