03f7804423b2ca296641421e0c4c83e443da5dbe
badandbest <91832803+badandbest@users.noreply.github.com>
2025-05-26 16:16:36 +0200

    BaseViewModel

---
+++ Code/Rendering/BaseViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using Sandbox;
+
+namespace Legacy;
+
+/// <summary>
+/// A common base we can use for weapons so we don't have to implement the logic over and over
+/// again.
+/// </summary>
+[Category( "ViewModel" )]
+[Title( "ViewModel" ), Icon( "pan_tool" )]
+public class BaseViewModel : AnimatedEntity
+{
+	/// <summary>
+	/// All active view models.
+	/// </summary>
+	public static List<BaseViewModel> AllViewModels = [];
+
+	public BaseViewModel()
+	{
+		AllViewModels.Add( this );
+	}
+
+	protected override void OnDestroy()
+	{
+		base.OnDestroy();
+
+		AllViewModels.Remove( this );
+	}
+
+	/// <summary>
+	/// Position your view model here.
+	/// </summary>
+	// [GameEvent.Client.PostCamera]
+	public virtual void PlaceViewmodel()
+	{
+		Position = Camera.Position;
+		Rotation = Camera.Rotation;
+	}
+
+	public override Sound PlaySound( string soundName, string attachment )
+	{
+		return Owner?.PlaySound( soundName, attachment ) ?? base.PlaySound( soundName, attachment );
+	}
+}
