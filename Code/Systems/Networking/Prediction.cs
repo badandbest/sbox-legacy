f3824bb5a4317fef13fada4ccf1e2f01de03b1e8
badandbest <91832803+badandbest@users.noreply.github.com>
2025-12-11 16:53:03 +0100

    Prediction class. Does nothing for now

---
+++ Code/Systems/Networking/Prediction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Legacy;
+
+public static class Prediction
+{
+	/// <summary>
+	/// Temporarily disables prediction.
+	///
+	/// <para>
+	/// Expected usage is like so:
+	/// <code>using ( Prediction.Off() )
+	/// {
+	/// 	// Code that needs prediction disabled here...
+	/// }
+	/// </code>
+	/// </para>
+	/// </summary>
+	public static IDisposable Off() => new EmptyDisposable();
+}
+
+file struct EmptyDisposable : IDisposable
+{
+	public void Dispose() { }
+}
