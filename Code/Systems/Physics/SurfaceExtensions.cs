c564e90080816bdd7ee22e0d6f3e4b7e3df70389
badandbest <91832803+badandbest@users.noreply.github.com>
2025-12-15 17:21:46 +0100

    Implement Surface.DoBulletImpact

---
+++ Code/Systems/Physics/SurfaceExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace Sandbox;
+
+/// <summary>
+/// Extensions for <see cref="Surface"/> that adds members from the old entity system.
+/// </summary>
+public static class SurfaceExtensions
+{
+	extension( Surface source )
+	{
+		public void DoBulletImpact( TraceResult tr )
+		{
+			var particle = source.PrefabCollection.BulletImpact;
+
+			var prefab = particle.Clone( tr.HitPosition, Rotation.LookAt( -tr.Normal ) );
+			prefab.PlaySound( source.SoundCollection.Bullet );
+
+			prefab.Flags |= GameObjectFlags.Hidden;
+		}
+	}
+}
