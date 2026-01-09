714a07c185cdc15c68ee806e06eb887be5c4ea1e
badandbest <91832803+badandbest@users.noreply.github.com>
2026-01-09 01:04:53 +0100

    ClientRpc, Net attributes

--- Code/Systems/Networking/Unimplemented/ClientRpcAttribute.cs
+++ Code/Systems/Networking/Unimplemented/ClientRpcAttribute.cs
@@ -1,9 +1,29 @@
-﻿using System;
+﻿using System.Linq;
 
 namespace Sandbox;
 
 /// <summary>
 /// A method that can be called by the server.
 /// </summary>
-[AttributeUsage( AttributeTargets.Method )]
-public class ClientRpcAttribute : Attribute;
+[CodeGenerator( CodeGeneratorFlags.WrapMethod | CodeGeneratorFlags.Static | CodeGeneratorFlags.Instance, "Sandbox.ClientRpcAttribute.Filter", 1 )]
+public class ClientRpcAttribute : Rpc.BroadcastAttribute
+{
+	public static void Filter( WrappedMethod m, params object[] argumentList )
+	{
+		using ( Game.ClientScope() )
+		{
+			var targets = argumentList.OfType<To?>().FirstOrDefault();
+
+			if ( !targets.HasValue )
+			{
+				m.Resume();
+				return;
+			}
+
+			using ( Rpc.FilterInclude( targets.Value.OfType<ClientEntity>().Select( x => x.Network.Owner ) ) )
+			{
+				m.Resume();
+			}
+		}
+	}
+}
--- Code/Systems/Networking/Unimplemented/NetAttribute.cs
+++ Code/Systems/Networking/Unimplemented/NetAttribute.cs
@@ -1,9 +1,6 @@
-﻿using System;
-
-namespace Sandbox;
+﻿namespace Sandbox;
 
 /// <summary>
 /// Mark a property as networked, so it is sent from the server to the client.
 /// </summary>
-[AttributeUsage( AttributeTargets.Property )]
-public class NetAttribute : Attribute;
+public class NetAttribute : SyncAttribute;
